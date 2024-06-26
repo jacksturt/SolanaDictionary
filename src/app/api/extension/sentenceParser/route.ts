1e149b5b15d1792bd1cfd28bebb883237b75375a
jacksturt <jack@sturt.io>
2024-06-26 16:38:42 -0600

    next config again

--- src/app/api/extension/sentenceParser/route.ts
+++ src/app/api/extension/sentenceParser/route.ts
@@ -1,6 +1,8 @@
 import cors from "~/utils/cors";
+import { db } from "~/server/db";
 
 export async function GET(request: Request) {
+
     console.log("Hello, Next.js!");
     return cors(request, new Response('Hello, Next.js!', {
       status: 200,
