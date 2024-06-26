61ad09a94999b522d9c5fbd0cb534afa50fc89d6
jacksturt <jack@sturt.io>
2024-06-26 16:29:57 -0600

    new cors

--- src/app/api/extension/sentenceParser/route.ts
+++ src/app/api/extension/sentenceParser/route.ts
@@ -1,11 +1,13 @@
+import cors from "~/utils/cors";
+
 export async function GET(request: Request) {
     console.log("Hello, Next.js!");
-    return new Response('Hello, Next.js!', {
+    return cors(request, new Response('Hello, Next.js!', {
       status: 200,
       headers: {
         'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Access-Control-Allow-Methods': '*',
+        'Access-Control-Allow-Headers': '*',
       }
-    });
+    }));
   }
